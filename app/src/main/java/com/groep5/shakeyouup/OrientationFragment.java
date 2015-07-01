0a6e93d8618d2fe67caf3de2a1c1a05b6afbab6d
Idwer Vollering <06000355@student.hhs.nl>
2015-07-01 23:33:59 +0200

    cleanup of OrientationFragment.java

--- app/src/main/java/com/groep5/shakeyouup/OrientationFragment.java
+++ app/src/main/java/com/groep5/shakeyouup/OrientationFragment.java
@@ -2,17 +2,9 @@ package com.groep5.shakeyouup;
 
 import android.app.Fragment;
 import android.os.Bundle;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class OrientationFragment extends Fragment {
 
-//    private DashboardActivity dashboardActivity;
     private Journey journey;
 
     @Override
@@ -28,18 +20,4 @@ public class OrientationFragment extends Fragment {
     public Journey getJourney() {
         return this.journey;
     }
-
-    // this line and all code beyond it can probably be deleted
-
-//    public OrientationFragment() {
-        // Required empty public constructor
-//    }
-
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        TextView textView = new TextView(getActivity());
-//        textView.setText(R.string.hello_blank_fragment);
-//        return textView;
-//    }
 }
